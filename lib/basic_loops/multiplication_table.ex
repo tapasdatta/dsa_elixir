6755a44634c5c6f7c49e693f8555a3bff6c62050
Tapas Datta <tapash.me@gmail.com>
2024-12-12 23:16:21 +0600

    Added multiplication table

--- lib/basic_loops/multiplication_table.ex
+++ lib/basic_loops/multiplication_table.ex
@@ -1,16 +1,21 @@
 defmodule MultiplicationTable do
-  # [1, 2]
-  # output: [1, 2], [2, 2]
-  # input:
-  def multiplication(list) do
-    {multiplication} = do_multiplication(tail, head, [])
-    IO.inspect(multiplication, label: "#{head}")
+  def print_table(list) do
+    print_rows(list, list)
   end
 
-  defp do_mulplication([], )
+  defp print_rows([], _original), do: :ok
 
-  defp do_multiplication([head | tail], number, math) do
-    # number * head
-    do_multiplication(tail, number, [number * head | math])
+  defp print_rows([row_head | row_tail], original) do
+    print_row(row_head, original)
+    # Add a newline after each row
+    IO.puts("")
+    print_rows(row_tail, original)
+  end
+
+  defp print_row(_multiplier, []), do: :ok
+
+  defp print_row(multiplier, [col_head | col_tail]) do
+    IO.write("#{multiplier * col_head}\t")
+    print_row(multiplier, col_tail)
   end
 end
