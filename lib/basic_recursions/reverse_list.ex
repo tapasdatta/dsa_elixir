56194d4b90ad0b3fe6e18b5e15a4b2e865cff6bf
Tapas Datta <tapash.me@gmail.com>
2024-12-18 00:48:01 +0600

    Added comments before for more readability

---
+++ lib/basic_recursions/reverse_list.ex
@@ -0,0 +1,22 @@
+defmodule ReverseList do
+  @moduledoc """
+  A module for reversing lists.
+  """
+
+  # Example: ReverseList.reverse([1]) => [1]
+  @spec reverse(list) :: list
+  def reverse(list) when length(list) == 1, do: list
+
+  # Example: ReverseList.reverse([1, 2, 3]) => [3, 2, 1]
+  @spec reverse(list) :: list
+  def reverse(list) do
+    do_reverse(list, [])
+  end
+
+  @spec do_reverse(list, list) :: list
+  defp do_reverse([], reverse_list), do: reverse_list
+
+  defp do_reverse([head | tail], reverse_list) do
+    do_reverse(tail, [head | reverse_list])
+  end
+end
--- lib/basic_recursions/sum_natural_num.ex
+++ lib/basic_recursions/sum_natural_num.ex
@@ -1,10 +1,21 @@
 defmodule SumN do
-  # Formula-Based Solution
+  @moduledoc """
+  Provides two methods to calculate the sum of the first `n` natural numbers.
+
+  ## Methods
+
+    * Formula-Based Solution: Uses the mathematical formula `n * (n + 1) / 2` to compute the sum.
+    * Tail-Recursive Solution: Uses a tail-recursive approach to compute the sum iteratively.
+  """
+
+  # Example: sum(5, "formula") -> 15
+  @spec sum(integer(), String.t()) :: integer()
   def sum(n, version) when version == "formula" do
     div(n * (n + 1), 2)
   end
 
-  # Tail-Recursive Solution
+  # Example: sum(5) -> 15
+  @spec sum(integer()) :: integer()
   def sum(n), do: do_sum(n, 0)
 
   defp do_sum(0, acc), do: acc
