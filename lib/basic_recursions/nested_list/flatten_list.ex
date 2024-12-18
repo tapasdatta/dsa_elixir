79c5ad2621d4e8a7c060f80c7e218540c4fe8db1
Tapas Datta <tapash.me@gmail.com>
2024-12-18 19:21:02 +0600

    Added comments

--- lib/basic_recursions/nested_list/flatten_list.ex
+++ lib/basic_recursions/nested_list/flatten_list.ex
@@ -1,4 +1,22 @@
 defmodule Flatten do
+  @moduledoc """
+  A module for flattening nested lists into a single list.
+
+  ## Examples
+
+      iex> Flatten.flatten([1, [2, 3], [4, [5, 6]], 7])
+      [1, 2, 3, 4, 5, 6, 7]
+
+      iex> Flatten.flatten([[], [1, [2, [3]]], 4])
+      [1, 2, 3, 4]
+
+      iex> Flatten.flatten([1, 2, 3])
+      [1, 2, 3]
+
+      iex> Flatten.flatten([])
+      []
+  """
+
   def flatten([]), do: []
 
   def flatten([head | tail]), do: flatten(head) ++ flatten(tail)
