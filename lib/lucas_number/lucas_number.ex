4c3f34879bd265f085463db6b5718c29c401487c
Tapas Datta <tapash.me@gmail.com>
2024-12-19 23:14:16 +0600

    Added lucas number sequence

---
+++ lib/lucas_number/lucas_number.ex
@@ -0,0 +1,33 @@
+defmodule LucasNumber do
+  @moduledoc """
+  Module for generating Lucas number sequences.
+  """
+
+  @doc """
+  Generates the Lucas number sequence up to the nth element.
+
+  ## Parameters
+  - n: The number of elements in the sequence to generate.
+
+  ## Examples
+
+      iex> LucasNumber.sequence(5)
+      [2, 1, 3, 4, 7]
+
+      iex> LucasNumber.sequence(-1)
+      []
+
+  """
+  @spec sequence(integer) :: list(integer)
+  def sequence(n) when n < 0, do: []
+
+  def sequence(n), do: do_sequence(n, [1, 2])
+
+  # Base case for recursion
+  @spec do_sequence(integer, list(integer)) :: list(integer)
+  def do_sequence(2, acc), do: Enum.reverse(acc)
+
+  def do_sequence(n, [a, b | _] = acc) do
+    do_sequence(n - 1, [a + b | acc])
+  end
+end
