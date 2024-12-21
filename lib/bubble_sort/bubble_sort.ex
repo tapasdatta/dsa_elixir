f734f2bf03b3e3bd1f346eb0869e795cf3b87124
Tapas Datta <tapash.me@gmail.com>
2024-12-21 22:29:01 +0600

    Added module doc

--- lib/bubble_sort/bubble_sort.ex
+++ lib/bubble_sort/bubble_sort.ex
@@ -1,4 +1,17 @@
 defmodule BubbleSort do
+  @moduledoc """
+  A module that provides a Bubble Sort implementation.
+
+  ## Examples
+
+      iex> BubbleSort.sort([4, 2, 7, 1, 3])
+      [1, 2, 3, 4, 7]
+
+      iex> BubbleSort.sort([5, 3, 8, 6, 2])
+      [2, 3, 5, 6, 8]
+  """
+
+  @spec sort(list(integer)) :: list(integer)
   def sort(n) when length(n) < 2, do: n
 
   def sort(list) do
