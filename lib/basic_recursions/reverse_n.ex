2a545b65ba2c8a6dd3ade7e0f1931b0267a8218e
Tapas Datta <tapash.me@gmail.com>
2025-02-25 18:07:31 +0600

    Updated some comments

--- lib/basic_recursions/reverse_n.ex
+++ lib/basic_recursions/reverse_n.ex
@@ -1,10 +1,13 @@
 defmodule ReverseN do
+  # Public function to reverse the sequence of numbers from n down to 1
   def reverse(n) when n >= 0 do
     do_reverse(n, [])
   end
 
+  # Private helper function to accumulate the reversed list
   defp do_reverse(0, reverseList), do: reverseList
 
+  # Recursive case for the private helper function
   defp do_reverse(n, reverseList) when n > 0 do
     do_reverse(n - 1, [n | reverseList])
   end
--- lib/basic_recursions/stack.ex
+++ lib/basic_recursions/stack.ex
@@ -1,6 +1,10 @@
 defmodule Stack do
+  # This function handles the base case where the input is 0.
+  # It simply returns :ok.
   def print(0), do: :ok
 
+  # This function prints the current number and then recursively
+  # calls itself with the number decremented by 1.
   def print(n) do
     IO.puts(n)
     print(n - 1)
