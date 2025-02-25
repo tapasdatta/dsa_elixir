7c7830c6c86cb4c8397f9ff0a3ebe6da9f915977
Tapas Datta <tapash.me@gmail.com>
2025-02-25 19:01:31 +0600

    rewrote some functions

--- lib/basic_recursions/nested_list/find_max_nested.ex
+++ lib/basic_recursions/nested_list/find_max_nested.ex
@@ -6,15 +6,7 @@ defmodule FindMaxNested do
 
       iex> FindMaxNested.max([1, [2, 3], [4, [5, 6]], 7])
       7
-
-      iex> FindMaxNested.max([[], [-1, -2], [-3, [-4, -5]], -6])
-      -1
-
-      iex> FindMaxNested.max([])
-      {:error, "Empty list"}
   """
-
-  def max([]), do: {:error, "Empty list"}
 
   def max(list) do
     find_max(list, 0)
@@ -28,9 +20,5 @@ defmodule FindMaxNested do
 
   defp find_max([head | tail], current_max) when is_number(head) do
     find_max(tail, Kernel.max(head, current_max))
-  end
-
-  defp find_max([_ | tail], current_max) do
-    find_max(tail, current_max)
   end
 end
