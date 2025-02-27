8305227ab5262e63897f555a02cb5ffc1717a168
Tapas Datta <tapash.me@gmail.com>
2025-02-27 18:33:30 +0600

    Refactored bubble sort

--- lib/bubble_sort/bubble_sort.ex
+++ lib/bubble_sort/bubble_sort.ex
@@ -11,27 +11,25 @@ defmodule BubbleSort do
       [2, 3, 5, 6, 8]
   """
 
-  @spec sort(list(integer)) :: list(integer)
-  def sort(n) when length(n) < 2, do: n
+  def sort(list), do: do_sort(list, length(list))
 
-  def sort(list) do
-    do_sort(length(list), list)
+  defp do_sort(list, 0), do: list
+
+  defp do_sort(list, n) do
+    list
+    |> bubble_pass([])
+    |> do_sort(n - 1)
   end
 
-  defp do_sort(0, list), do: list
+  defp bubble_pass([], acc), do: Enum.reverse(acc)
 
-  defp do_sort(n, [head | tail]) do
-    sorted_list = bubble_sort(tail, head, [])
-    do_sort(n - 1, sorted_list)
+  defp bubble_pass([x], acc), do: [x | acc]
+
+  defp bubble_pass([x, y | tail], acc) when x > y do
+    bubble_pass([y | tail], [x | acc])
   end
 
-  defp bubble_sort([], current_head, acc), do: Enum.reverse([current_head | acc])
-
-  defp bubble_sort([head | tail], current_head, acc) when current_head < head do
-    bubble_sort(tail, head, [current_head | acc])
-  end
-
-  defp bubble_sort([head | tail], current_head, acc) do
-    bubble_sort(tail, current_head, [head | acc])
+  defp bubble_pass([x, y | tail], acc) do
+    bubble_pass([x | tail], [y | acc])
   end
 end
