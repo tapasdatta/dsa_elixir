a2d08ef7ef451dc1dbe7a621989f509e8f0f6950
Tapas Datta <tapash.me@gmail.com>
2025-03-08 16:59:27 +0600

    Improved BFS code performance.

--- lib/breadth_first_search/breadth_first_search.ex
+++ lib/breadth_first_search/breadth_first_search.ex
@@ -8,32 +8,52 @@ defmodule BreadthFirstSearch do
     alice  bob   claire
     |    /  \       /   \
   peggy anuj peggy thom jonny
+
+  graph = %{
+    "tapas" => ["alice", "bob", "claire"],
+    "bob" => ["anuj", "peggy"],
+    "alice" => ["peggy"],
+    "claire" => ["thom", "jonny"],
+    "peggy" => [],
+    "thom" => [],
+    "jonny" => []
+  }
   """
 
-  # Example output: ["tapas", "alice", "bob", "claire", "peggy", "anuj", "thom", "jonny"]
+  @doc """
+  Performs BFS on a graph starting from a given node.
 
-  def search() do
-    graph = %{
-      "tapas" => ["alice", "bob", "claire"],
-      "bob" => ["anuj", "peggy"],
-      "alice" => ["peggy"],
-      "claire" => ["thom", "jonny"],
-      "peggy" => [],
-      "thom" => [],
-      "jonny" => []
-    }
+  ## Parameters
+  - graph: A map where keys are nodes and values are lists of neighbors.
+  - start: The starting node.
 
-    do_search(graph, ["tapas"], [], [])
+  ## Returns
+  - A list of nodes in BFS order.
+
+  ## Examples
+      iex> graph = %{"tapas" => ["alice", "bob", "claire"], "bob" => ["anuj", "peggy"], "alice" => ["peggy"], "claire" => ["thom", "jonny"], "anuj" => [], "peggy" => [], "thom" => [], "jonny" => []}
+      iex> BreadthFirstSearch.search(graph, "tapas")
+      ["tapas", "alice", "bob", "claire", "peggy", "anuj", "thom", "jonny"]
+  """
+  def search(graph, start) do
+    queue = :queue.from_list([start])
+
+    do_search(graph, queue, MapSet.new(), [])
   end
 
-  defp do_search(_graph, [], _visited, result), do: Enum.reverse(result)
+  defp do_search(graph, queue, visited, result) do
+    case :queue.out(queue) do
+      {{:value, current}, new_queue} ->
+        if MapSet.member?(visited, current) do
+          do_search(graph, new_queue, visited, result)
+        else
+          neighbours = Map.get(graph, current, [])
+          new_queue = Enum.reduce(neighbours, new_queue, &:queue.in/2)
+          do_search(graph, new_queue, MapSet.put(visited, current), [current | result])
+        end
 
-  defp do_search(graph, [current | queue], visited, result) do
-    if current in visited do
-      do_search(graph, queue, visited, result)
-    else
-      neighbours = Map.get(graph, current, [])
-      do_search(graph, queue ++ neighbours, [current | visited], [current | result])
+      {:empty, _} ->
+        Enum.reverse(result)
     end
   end
 end
